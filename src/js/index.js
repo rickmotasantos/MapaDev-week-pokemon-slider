e2efb6ded224e9181983d88fb928ccb673366a1f
Rick Mota <ricardorickricardosmt@gmail.com>
2022-08-06 18:26:50 -0300

    adicionei na pasta style.css, na classe cartao, o parametro opacity - 0

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,8 @@
-/*
-    OBJETIVO 1 - Quando clicarmos na seta de avançar temos que motrar o próximo cartão da lista
-        - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
-
-        - passo 2 - dar um jeito de identificar o clique do usuario da seta avançar
-
-        - passo 3 - fazer aparecer o proxima  cartão da lista
-
-        - passo 4 - buscar o cartão que está selecionado e esconder
-
-    
-*/
-
 const btnAvancar = document.getElementById('btn-avancar');
 const btnVoltar = document.getElementById('btn-voltar');
 const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
+
 
 function esconderCartaoSelecionado(){
     const cartaoSelecionado = document.querySelector(".selecionado");
@@ -36,15 +24,7 @@ btnAvancar.addEventListener('click', function(){
     
 });
     
-// OBJETIVO 2 - Quando clicarmos na seta de voltar temos que motrar o cartão anterior da lista
 
-//         - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-
-//         - passo 2 - dar um jeito de identificar o clique do usuario da seta voltar
-
-//         - passo 3 - fazer aparecer o cartão anterior da lista
-
-//         - passo 4 - buscar o cartão que está selecionado e esconder
 
 btnVoltar.addEventListener('click', function(){
     if(cartaoAtual === 0) return;
@@ -52,10 +32,7 @@ btnVoltar.addEventListener('click', function(){
     esconderCartaoSelecionado();
 
     cartaoAtual--;
-    mostrarCartao();
-
-    //esconder o cartão selecionado
-    
+    mostrarCartao();    
     
 });
 
